18d1f91a01822290c135639307b2a73ca0ee9b67
Ankush Singh <ankushsahil45@gmail.com>
2023-01-04 19:07:33 +0530

    added save func

---
+++ src/utils/saveAs.js
@@ -0,0 +1,21 @@
+export function saveAs(blob, fileName) {
+  var elem = window.document.createElement("a");
+  elem.href = blob;
+  elem.download = fileName;
+  elem.style = "display:none";
+  (document.body || document.documentElement).appendChild(elem);
+  if (typeof elem.click === "function") {
+    elem.click();
+  } else {
+    elem.target = "_blank";
+    elem.dispatchEvent(
+      new MouseEvent("click", {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+  }
+  URL.revokeObjectURL(elem.href);
+  elem.remove();
+}
